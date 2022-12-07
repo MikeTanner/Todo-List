48f8a2057856967a3b503668c3702cc9262fef41
Mike Tanner <miketannercs@gmail.com>
2022-12-07 17:34:45 -0600

    init project

---
+++ dist/bundle76e5fe988a97d0cdd9dd.js
@@ -0,0 +1,10 @@
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+console.log("test");
+
+/******/ })()
+;
+//# sourceMappingURL=bundle76e5fe988a97d0cdd9dd.js.map
